4c8f1a7041c9d13659936c399990a46456c15b87
Luigi Croce <luicrodez@iesxulianmagarinos.org>
2024-03-19 12:06:11 +0100

    clase 19 de marzo

---
+++ Proyecto/citasmod.py
@@ -0,0 +1,78 @@
+
+def leer(nombre):
+    try:
+        f=open(nombre, 'r')
+    except FileNotFoundError:
+        print('no existe el fichero')
+    else:
+        print(f.read())
+        f.close()
+    return
+
+
+def escribir(nombre):
+   #toma de datos  
+    dni= input("Introduzca su dni:")
+    mes= input("Introduzca el mes :")
+    dia= input("Introduzca el dia:")
+    hora= input("Introduzca la hora:")
+    especialidad= input("Introduzca la especialidad:")
+    #escribir en fichero
+    citas=open(nombre, 'a+')
+    citas.write(dni + ";" + mes + ";"+ dia + ";" + hora + ";"+ especialidad + "\n")
+    citas.close
+    return
+
+def buscar(fichero):
+    dnib="44844484F"
+    with open(fichero, 'r') as f:
+        for linea in f:
+            if dnib in linea:
+                print(linea)
+    return
+
+def eliminar (fichero):
+    dnib="44844484F"
+    d=leer(fichero)
+    with open(fichero, 'w') as f:
+        for linea in d :
+            if dnib not in linea:
+                f.write(linea)
+    return
+
+
+def menu():
+    print("============================")
+    print("Aplicacion Banco de Negreira")
+    print("============================")
+    print("1 - Consultar Citas")
+    print("2 - Añadir cita nueva")
+    print("3 - Buscar una Cita")
+    print("4 - Eliminar Citas")
+    print("0 - Salir")
+
+    op=input("Introduzca La Opcion Deseada:")
+    return op
+
+
+
+def gestion():
+    fichero="Banco.csv"
+    while True:
+        opcion=menu()
+        if opcion == '1':
+            leer(fichero)
+        elif opcion == '2':
+            escribir(fichero)
+        elif opcion == '3':
+            buscar(fichero)
+        elif opcion == '4':
+            eliminar(fichero)
+        else:
+            break
+
+    
+gestion()
+
+
+
